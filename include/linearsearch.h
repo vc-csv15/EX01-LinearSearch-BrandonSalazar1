8a5c9ef3f36d6462d4288a11dcac43eb0da5e9c2
Brandon Salazar <brandon_salazar3@my.vcccd.edu>
2019-10-07 19:23:45 -0700

    yes

--- include/linearsearch.h
+++ include/linearsearch.h
@@ -9,6 +9,9 @@
 
 namespace edu { namespace vcccd { namespace vc { namespace csv15 {
 
+    using namespace std;
+
+
                 template <class T>
                 int64_t find(const T& value, T array[], size_t size) {
                     // put your code here
